2c395863542b4271dc4db2313442a5c75b77c8c1
SanderTLN <62286818+SanderTLN@users.noreply.github.com>
2021-05-14 14:06:38 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -4,30 +4,33 @@
 	<link href="https://fonts.googleapis.com/css?family=Averia+Serif+Libre|Noto+Serif|Tangerine" rel="stylesheet">
 	<link rel="stylesheet" href="css/style.css">
 	<meta charset="UTF-8">
-	<title>FoodBlog</title>
+	<title>AdminBlog</title>
 </head>
-<body img src="img/logo.png" alt="Blogception logo">
+<body>
+    <style>
+        body {
+            background-image: url(img/logo.jpg);
+            background-attachment: fixed;
+        }
+    </style>
 	<div class="container">
 		<div class="navbar">
 			<div class="logo_div">
-				<a href="index.php"><h1>FoodBlog</h1></a>
+				<a href="index.php"><h1>AdminBlog</h1></a>
 			</div>
 			<ul>
 			  <li><a class="active" href="index.php">Home</a></li>
 			  <li><a href="#contact">Contact</a></li>
 			  <li><a href="#about">About</a></li>
-              <li><a href="#login">Admin</a></li>
+              <li><a href="admin.php">Log in</a></li>
 			</ul>
 		</div>
 		<div class="content">
 			<h2 class="content-title">Recent Articles</h2>
 			<hr>
 		</div>
-        <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste eius laudantium, quas velit quis corrupti mollitia odio saepe excepturi sequi ex unde illum enim fuga adipisci! Repellendus veritatis minus quisquam.
-        </div>
 		<div class="footer">
-			<p>Admin Blog &copy; <?php echo date('Y'); ?></p>
+			<p>MyBlog &copy; <?php echo date('Y'); ?></p>
 		</div>
 	</div>
 </body>
