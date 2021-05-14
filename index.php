a3f9b973cd4898bab66864190395514cf9368330
SanderTLN <62286818+SanderTLN@users.noreply.github.com>
2021-05-14 11:45:26 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="https://fonts.googleapis.com/css?family=Averia+Serif+Libre|Noto+Serif|Tangerine" rel="stylesheet">
+	<link rel="stylesheet" href="css/style.css">
+	<meta charset="UTF-8">
+	<title>FoodBlog</title>
+</head>
+<body img src="img/logo.png" alt="Blogception logo">
+	<div class="container">
+		<div class="navbar">
+			<div class="logo_div">
+				<a href="index.php"><h1>FoodBlog</h1></a>
+			</div>
+			<ul>
+			  <li><a class="active" href="index.php">Home</a></li>
+			  <li><a href="#contact">Contact</a></li>
+			  <li><a href="#about">About</a></li>
+              <li><a href="#login">Admin</a></li>
+			</ul>
+		</div>
+		<div class="content">
+			<h2 class="content-title">Recent Articles</h2>
+			<hr>
+		</div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste eius laudantium, quas velit quis corrupti mollitia odio saepe excepturi sequi ex unde illum enim fuga adipisci! Repellendus veritatis minus quisquam.
+        </div>
+		<div class="footer">
+			<p>Admin Blog &copy; <?php echo date('Y'); ?></p>
+		</div>
+	</div>
+</body>
+</html>
